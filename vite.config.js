3ca4cf7bd6e3ee0cf2aadc3f1a77c53fbced67b4
stebackman <stebackman@gmail.com>
2024-11-07 13:45:01 +0200

    testaus

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/02-unicafe-step5",
+  base: "/02-unicafe-step5/",
   plugins: [react()],
 });
